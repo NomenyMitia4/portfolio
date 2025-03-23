2cbe9e11c0ede318062926daa9b8c3a9ccac2483
Mikle <miklenomeny@gmail.com>
2025-03-23 18:01:08 +0300

    first push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import style from "./home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faFacebook, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faBurger, faBars } from "@fortawesome/free-solid-svg-icons";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Home()
+{
+  return(
+    <>
+      <div className={style.header}></div>
+      <div className={style.home}>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        {/* Container starts here */}
+        <div className={style.container}>
+          <div className={style.menu}>
+            <div className={style.item}>Home</div>
+            <div className={style.item}>About</div>
+            <div className={style.item}>Services</div>
+            <div className={style.item}>Contact</div>
+          </div>
+          <div className={style.burger}>
+            <FontAwesomeIcon icon={faBars} size="2x" width={30} height={30}/>
+          </div>
+          <h2>Hi, I am</h2>
+          <h1>Nomeny Mitia</h1>
+          <p>FullStack Developer</p>
+          <div className={style.links}>
+            <div className={style.btn}>
+              <button className={style.btn_link}><a href="http://">DOWNLOAD CV</a></button>
+              <div className={style.social_media}>
+                <p>Join me here</p>
+                <div className={style.social_media_icons}>
+                  <FontAwesomeIcon icon={faFacebook} size="2x" width={40} height={40}/>
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" width={40} height={40}/>
+                  <FontAwesomeIcon icon={faGithub} size="2x" width={40} height={40}/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        {/* Container ends here */}
+
+        <div className={style.scrolldown}>
+          <div className={style.services}></div>
+
+        </div>
+      </div>
+    </>
+  )
 }
