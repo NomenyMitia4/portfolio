a2e44fcf91bd05adbb8b9f72a83e9df5f5096a9b
Mikle <miklenomeny@gmail.com>
2025-03-23 22:35:42 +0300

    Services section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import style from "./home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faFacebook, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
@@ -30,9 +31,9 @@ export default function Home()
               <div className={style.social_media}>
                 <p>Join me here</p>
                 <div className={style.social_media_icons}>
-                  <FontAwesomeIcon icon={faFacebook} size="2x" width={40} height={40}/>
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" width={40} height={40}/>
-                  <FontAwesomeIcon icon={faGithub} size="2x" width={40} height={40}/>
+                  <FontAwesomeIcon className={style.icon} icon={faFacebook} size="2x" width={40} height={40}/>
+                  <FontAwesomeIcon className={style.icon} icon={faLinkedin} size="2x" width={40} height={40}/>
+                  <FontAwesomeIcon className={style.icon} icon={faGithub} size="2x" width={40} height={40}/>
                 </div>
               </div>
             </div>
@@ -41,8 +42,43 @@ export default function Home()
         {/* Container ends here */}
 
         <div className={style.scrolldown}>
-          <div className={style.services}></div>
-
+          <div className={style.services}>
+            <div className={style.arrow_left}>
+              <FontAwesomeIcon className={style.arrow} icon={faArrowAltCircleLeft} size="2x" width={70} height={70}/>
+            </div>
+            <div className={style.services_sections}>
+              <div className={style.caption}>
+                <h2>SERVICES</h2>
+                <p>Struggling to keep up with the fast-paced world of technology?</p>
+              </div>
+              <div className={style.list_services}>
+                <div className={style.service}>
+                  <div className={style.service_logo}>
+                    <img src={"/image/python.jpg"} alt="python logo" width={150} height={150}/>
+                  </div>
+                  <p className={style.title}>Backend Developer</p>
+                  <p className={style.description}>In a fast-paced tech world, a skilled Python Backend Developer ensures your applications are built on a strong, scalable foundation. Whether launching a new product, scaling up, or optimizing performance, a Python developer delivers precision and expertise to help you succeed.</p>
+                </div>
+                <div className={style.service}>
+                  <div className={style.service_logo}>
+                    <img src={"/image/next.jpg"} alt="python logo" width={150} height={150}/>
+                  </div>
+                  <p className={style.title}>Frontend Developer</p>
+                  <p className={style.description}>In a world where user experience is everything, a Next.js Developer can transform your web applications into fast, dynamic, and SEO-friendly platforms. Built on React, Next.js is the ultimate framework for modern web development, offering server-side rendering, static site generation, and seamless scalability.</p>
+                </div>
+                <div className={style.service}>
+                  <div className={style.service_logo}>
+                    <img src={"/image/ai.jpg"} alt="python logo" width={150} height={150}/>
+                  </div>
+                  <p className={style.title}>AI implementation</p>
+                  <p className={style.description}>The future belongs to those who embrace AI today. By integrating AI into your business, you can unlock new opportunities, solve complex problems, and create smarter, more efficient systems. Don’t get left behind—harness the power of AI to drive innovation and growth.</p>
+                </div>
+              </div>
+            </div>
+            <div className={style.arrow_right}>
+            <FontAwesomeIcon className={style.arrow} icon={faArrowAltCircleRight} size="2x" width={70} height={70}/>
+            </div>
+          </div>
         </div>
       </div>
     </>
