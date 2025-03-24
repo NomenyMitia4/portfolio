8314d61325bbe61c6e4a6c9654b4f7e005cfa23b
Mikle <miklenomeny@gmail.com>
2025-03-24 13:20:50 +0300

    &apos rectification

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function About()
                 <div className={`${style.container} ${style.left_container}`}>
                     <Image src={"/image/esmia_logo.jpg"} alt="ESMIA logo"/>
                     <div className={style.textbox}>
-                        <h2>Ecole Supérieur de Management et d'Informatique Appliquée</h2>
+                        <h2>Ecole Supérieur de Management et d&aposInformatique Appliquée</h2>
                         <small>In progress</small>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis alias aut repudiandae quia sed, molestias dignissimos esse! Sunt consectetur aperiam enim velit temporibus, omnis assumenda mollitia earum in, ab quis.</p>
                         <span className={style.left_container_arrow}></span>
@@ -21,7 +21,7 @@ export default function About()
                 <div className={`${style.container} ${style.right_container}`}>
                     <Image src={"/image/esmia_logo.jpg"} alt="ESMIA logo"/>
                     <div className={style.textbox}>
-                        <h2>Ecole Supérieur de Management et d'Informatique Appliquée</h2>
+                        <h2>Ecole Supérieur de Management et d&apos Informatique Appliquée</h2>
                         <small>2021 - 2024</small>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis alias aut repudiandae quia sed, molestias dignissimos esse! Sunt consectetur aperiam enim velit temporibus, omnis assumenda mollitia earum in, ab quis.</p>
                         <span className={style.right_container_arrow}></span>
